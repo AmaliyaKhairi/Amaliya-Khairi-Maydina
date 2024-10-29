16385c3151b93c3f38c1b065e9de5b3079021a80
AmaliyaKhairi <akunamaliya@gmail.com>
2024-10-29 12:43:12 +0700

    Add files via upload

---
+++ printa1.py
@@ -0,0 +1,6 @@
+print("kalkulator luas persegi panjang")
+panjang = input("masukkan panjang :")
+lebar = input("masukkan lebar :")
+luas = int (panjang ) *int(lebar) 
+print("luas persegi adalah", luas)
+
